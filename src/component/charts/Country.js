81e9a86e549c692a739cb18b1398abf54484435f
loseahmil <lhs363447@gmail.com>
2024-04-04 12:25:34 +0900

    Feat : 국가 차트 옆 상위 3개 국가 텍스트 출력

--- src/component/charts/Country.js
+++ src/component/charts/Country.js
@@ -6,14 +6,14 @@ const Country = () => {
   const [chartData, setChartData] = useState([]);
 
   useEffect(() => {
-    // 손승아, axios.get 통해 spring boot에서 데이터 받아오기, 20240401
     axios
       .get("http://localhost:8081/admin/country")
       .then((response) => {
         const data = response.data;
         console.log("백분율 값과 국가 이름:", data);
         const filteredData = data.filter((item) => item.nation !== "ADMIN"); // admin 국가 필터링
-        setChartData(filteredData);
+        const sortedData = filteredData.sort((a, b) => b.userCount - a.userCount); // 내림차순 정렬
+        setChartData(sortedData);
       })
       .catch((error) => {
         console.error("데이터 요청 실패", error);
@@ -26,7 +26,7 @@ const Country = () => {
       type: "donut",
     },
     labels: chartData.map((item) => item.nation),
-    colors: ["#5f76e8", "#01caf1", "#172072"], // 색상 설정
+    colors: ["#29635f", "#008953", "#bdda57"], // 색상 설정
     dataLabels: {
       enabled: true,
       formatter: function (val, opts) {
@@ -46,9 +46,14 @@ const Country = () => {
             show: true,
             name: {
               show: true, // 이름 표시 여부
+              fontSize: '30px'
             },
             value: {
               show: true, // 값 표시 여부
+              fontSize: '20px',
+              formatter: function (val) {
+                return val + '명'; // 라벨 값 뒤에 "명" 추가
+              }
             }
           }
         }
@@ -56,32 +61,28 @@ const Country = () => {
     }
   };  
 
-  const chartSeries = chartData.map((item) => item.userCount);
-
   return (
     <div className="flex">
       <div className="ml-7 mr-7 mt-10 h-full w-3/4">
         <ReactApexChart
           options={chartOptions}
-          series={chartSeries}
+          series={chartData.map((item) => item.userCount)}
           type="donut"
           height={350}
         />
       </div>
-      <div className="col-span-1">
+      <div className="col-span-1" style={{ width: "30%" }}>
         <div>
           <div className="px-3 pt-3 text-xl font-bold leading-6">
             국가별 이용자 수
           </div>
-          <div className="px-6 text-sm font-medium leading-6">
-            여기에 상위 1위, 해당 퍼센트 입력
-          </div>
-          <div className="px-6 text-sm font-medium leading-6">
-            여기에 상위 2위, 해당 퍼센트 입력
-          </div>
-          <div className="px-6 text-sm font-medium leading-6">
-            여기에 상위 3위, 해당 퍼센트 입력
-          </div>
+          <br></br>
+          <br></br>
+          {chartData.slice(0, 3).map((item, index) => (
+            <div key={index} className="px-6 text-sm font-medium leading-6" style={{ fontFamily: "Arial", fontSize: "17px", fontWeight: "bold" }}>
+              {`${index + 1}위 : ${item.nation} - ${item.userPercentage.toFixed(2)}%`}
+            </div>
+          ))}
         </div>
       </div>
     </div>
