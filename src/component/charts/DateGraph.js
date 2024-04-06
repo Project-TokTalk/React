145c41aab883ea45aced2c1b5705f26157f5bd2d
loseahmil <lhs363447@gmail.com>
2024-04-06 10:54:34 +0900

    Feat: 백엔드 데이터와 연결 후 월별 사용자 증가 추이 그래프 생성

--- src/component/charts/DateGraph.js
+++ src/component/charts/DateGraph.js
@@ -1,52 +1,75 @@
-import React from "react";
-import ReactApexChart from "react-apexcharts";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Chart from 'react-apexcharts';
 
 const DateGraph = () => {
-  // 하드코딩된 예시 데이터
-  const series = [{
-    name: 'Count',
-    data: [1, 2, 4, 6, 8, 6, 12, 1]
-  }];
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://localhost:8081/admin/date');
+      const sortedData = response.data.sort((a, b) => {
+        const [aYear, aMonth] = a.year_month.split('-').map(Number);
+        const [bYear, bMonth] = b.year_month.split('-').map(Number);
+        return aYear === bYear ? aMonth - bMonth : aYear - bYear;
+      });
+      setData(sortedData);
+      console.log("월별 사용자 증가 추이 : ", sortedData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  if (data.length === 0) return null;
+
+  // 누적 데이터 계산
+  let cumulativeCounts = 0;
+  const cumulativeData = data.map(item => {
+    cumulativeCounts += item.count;
+    return cumulativeCounts;
+  });
+
+  const labels = data.map(item => item.year_month);
+
   const options = {
     chart: {
-      id: 'date-chart',
-      toolbar: {
-        show: false
-      }
+      id: 'user-join-date-chart',
     },
     xaxis: {
-      categories: ['2023-09', '2023-10', '2023-11', '2023-12', '2024-01', '2024-02', '2024-03', '2024-04']
+      categories: labels,
     },
-    yaxis: {
-      title: {
-        text: 'Count'
-      }
-    },
-    stroke: {
-      curve: 'smooth'
+    dataLabels: {
+      enabled: true, // 점에 레이블을 표시
     },
     fill: {
-      type: 'gradient',
-      gradient: {
-        type: 'solid',
-        color: '#87CEEB',
-        opacity: 0.3
-      }
+      color: 'rgba(135, 206, 235, 0.5)', // 투명한 하늘색으로 설정
     },
-    tooltip: {
-      enabled: true,
-      shared: true,
+    stroke: {
+      curve: 'straight', // 선의 형태 (예: 'smooth', 'straight', 'stepline')
+      colors: ['#5CACEE'], // 선의 색상
+      width: 4, // 선의 두께
     },
   };
 
+  const series = [
+    {
+      name: '누적 사용자 수',
+      data: cumulativeData,
+    },
+  ];
+
   return (
-    <div className="text-2xl font-bold mb-4" style={{textAlign:"center", color:"#002267", textDecoration:"underline", textDecorationStyle:"dotted", marginTop:"50px"}}>
-      <h2>이용자 수 증감 추이</h2>
-      <div className="date-graph" style={{ maxWidth: "800px", marginLeft:"40px", marginTop:"10px" }} >
-        <ReactApexChart options={options} series={series} type="area" height={300} />
+    <div style={{ width: '90%', margin: '0 auto', marginTop: "50px" }}> {/* 80%의 너비를 가지고 중앙 정렬 */}
+      <div className="mb-4 text-center text-2xl font-bold text-color_b underline decoration-dotted">
+        월별 사용자 증가 추이
       </div>
+      <Chart options={options} series={series} type="area" height={300} />
     </div>
   );
-}
+};
 
 export default DateGraph;
