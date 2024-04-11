838ab42fea8b02336553c585466908fb01fd89bc
hummingbr <hummingbr711@gmail.com>
2024-04-11 09:30:40 +0900

    Feat : 챗봇 로그 기능 추가(동일 유저에서 한, 영 기록 함께 조회 가능)

--- src/component/table/ChatRecord.js
+++ src/component/table/ChatRecord.js
@@ -1,21 +1,100 @@
-import "../charts/Font.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import ChatbotHeader from "../layout/ChatbotHeader";
 
 const ChatRecord = () => {
-  const user_IdList = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-    22, 23, 24, 25, 26, 27, 28, 29, 30,
-  ];
-  const chat_IdList = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-    22, 23, 24, 25, 26, 27, 28, 29, 30,
-  ];
+  const [Kodata, setKodata] = useState([]);
+  const [Endata, setEndata] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [sortedChats, setSortedChats] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response1 = await axios.get("http://localhost:8081/admin/kochat");
+      const response2 = await axios.get("http://localhost:8081/admin/enchat")
+      setKodata(response1.data); 
+      setEndata(response2.data); 
+
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+
+  };
+
+  function countValuesInMap(map) {
+    return Object.keys(map).reduce((totalCount, key) => {
+        // 현재 키의 배열의 길이를 합산, count에 추가
+        return totalCount + map[key].length;
+    }, 0); // 초기값 0
+  }
+  function countAllMap (){
+    const totalValues = countValuesInMap(Kodata) + countValuesInMap(Endata);
+    return totalValues;
+  }
+
+  const WrongAnswerK = "죄송합니다. 이해하지 못했습니다. 키워드로 질문하거나 좀 더 정확하게 말씀해주세요. 예 : 제품 등록"
+  const WrongAnswerE = "I'm sorry. I didn't understand. Please ask me a ke…e more accurately. Example : Product Registration"
+
+  function countAnswersWithSpecificValue(map, specificValue) {
+    let count = 0;
+
+    // 맵의 각 키에 해당하는 값을 순회하면서 조건을 확인합니다.
+    Object.values(map).forEach(items => {
+        // 각 값들의 answer 속성이 특정 문자열과 동일한지 확인하여 카운트를 증가시킵니다.
+        items.forEach(item => {
+            if (item.answer === specificValue) {
+                count++;
+            }
+        });
+    });
+    
+    return count;
+  }
+
+  const CountWrongAnswer = () => {
+    const Answer = countAnswersWithSpecificValue(Kodata, WrongAnswerK)+countAnswersWithSpecificValue(Endata, WrongAnswerE);
+    return Answer;
+  }
+
+  const handleUserClick = (user) => {
+    setSelectedUser(user); // 사용자를 선택한 상태로 변경
+
+    let userChats = [];
+  
+    // Kodata[user]와 Endata[user]가 모두 존재하는 경우
+    if (Kodata[user] && Endata[user]) {
+      userChats = [...Kodata[user], ...Endata[user]]; // 두 배열을 합침
+    } 
+    // Kodata[user]만 존재하는 경우
+    else if (Kodata[user]) {
+      userChats = [...Kodata[user]]; // Kodata[user]만 사용
+    } 
+    // Endata[user]만 존재하는 경우
+    else if (Endata[user]) {
+      userChats = [...Endata[user]]; // Endata[user]만 사용
+    }
+    
+    setSortedChats(userChats); // 선택된 사용자의 채팅 기록을 설정
+  }
+
+  const allKeys = [...new Set([...Object.keys(Kodata), ...Object.keys(Endata)])];
+  
+
+
 
   // 04/05
   // 각각 반복문으로 바꿔서 전체 코드 길이 줄임
 
   return (
     <>
+      {/* {console.log(Kodata)}
+      {console.log(Endata)} */}
+      {console.log(sortedChats)}
+      {console.log(allKeys)}
       <ChatbotHeader />
       <div className="flex h-screen w-full bg-white text-gray-800 antialiased" style={{fontFamily: 'Pretendard-Regular'}}>
         <div className="flex h-4/5 flex-1 flex-col">
@@ -46,29 +125,28 @@ const ChatRecord = () => {
                 </div>
               </h1>
               <ul className="flex flex-col overflow-auto">
-                {user_IdList.map((id_chatuser) => (
-                  <li key={id_chatuser} className="flex text-gray-700 ">
+                {allKeys.map((key, index) => (
+                  <li key={index} onClick={() => handleUserClick(key)}>
                     <article
-                      tabindex="0"
-                      className="mb-2 flex w-full cursor-pointer rounded-md border bg-white p-3 text-gray-700 hover:border-blue-300 focus:border-blue-500 focus:outline-none"
+                      tabIndex="0"
+                      className="mb-2 flex cursor-pointer rounded-md border bg-white p-3 text-gray-700 hover:border-green-500 focus:border-green-500 focus:outline-none"
                     >
                       <span className="flex-none pr-2 pt-1">
-                        <image
+                        <img
                           className="h-8 w-8 rounded-md"
                           src="https://raw.githubusercontent.com/bluebrown/tailwind-zendesk-clone/master/public/assets/avatar.png"
+                          alt="User Avatar"
                         />
                       </span>
-
-                      <div className="flex-1 w-full">
+                      <div className="flex-1">
                         <header className="mb-1">
-                          사용자 아이디 {id_chatuser}
+                          {key} <span className="font-semibold">Chat</span> on
                         </header>
                         <p className="text-gray-600">
-                          사용자의 마지막 채팅 내용 혹은 주요 채팅? <br />
-                          몰라 알아서 하슈 {id_chatuser}
+                          {/* Additional content here */}
                         </p>
                         <footer className="mt-2 text-sm text-gray-500">
-                          마지막 채팅 시간으로 수정 {id_chatuser}
+                          Friday 22:16
                         </footer>
                       </div>
                     </article>
@@ -95,7 +173,7 @@ const ChatRecord = () => {
                   <ul className="flex">
                     <li>
                       <button className="flex w-24 cursor-default flex-col items-center rounded-md border bg-white p-2 focus:bg-yellow-200 focus:outline-none">
-                        <p className="text-lg font-semibold">32</p>
+                        <p className="text-lg font-semibold">{countAllMap()}</p>
                         <p className="text-sm uppercase text-gray-600">Count</p>
                       </button>
                     </li>
@@ -116,14 +194,14 @@ const ChatRecord = () => {
                   <ul className="flex">
                     <li>
                       <button className="flex w-24 cursor-default flex-col items-center rounded-l-md border border-r-0 bg-white p-2 focus:bg-yellow-200 focus:outline-none">
-                        <p className="text-lg font-semibold">30</p>
+                        <p className="text-lg font-semibold">{countAllMap()-CountWrongAnswer()}</p>
                         <p className="text-sm uppercase text-gray-600">Good</p>
                       </button>
                     </li>
 
                     <li>
                       <button className="flex w-24 cursor-default flex-col items-center rounded-r-md border bg-white p-2 focus:bg-yellow-200 focus:outline-none">
-                        <p className="text-lg font-semibold">2</p>
+                        <p className="text-lg font-semibold">{CountWrongAnswer()}</p>
                         <p className="text-sm uppercase text-gray-600">Bad</p>
                       </button>
                     </li>
@@ -134,7 +212,14 @@ const ChatRecord = () => {
               <header className="flex items-center border-t bg-white px-4 py-1">
                 <div className="flex h-8 items-center justify-center">
                   <h2 id="content-caption" className="font-semibold">
-                    사용자별 채팅 내역
+                  {selectedUser === null && (
+                    <p>채팅 기록을 확인해주세요.</p>
+                  )}
+
+                  {selectedUser !== null && (
+                    <p>{selectedUser} 님의 채팅 내역</p>
+                  )}
+                    
                   </h2>
                 </div>
               </header>
@@ -158,51 +243,23 @@ const ChatRecord = () => {
                 </thead>
                 <tbody className="flex min-h-0 w-full flex-1 flex-col px-4">
                   <div className="flex h-full w-full flex-col overflow-auto">
-                    {/* 04/05
-                    그냥 Y/N 보여줄라고 조건식 넣음 ^^ */}
-                    {chat_IdList.map((id_chatlist) =>
-                      id_chatlist % 2 === 1 ? (
-                        <tr
-                          role="row"
-                          className="flex border-b hover:bg-blue-100"
-                        >
-                          <td className="flex w-64 items-center justify-center px-1 py-3">
-                            2024년 04월 01일 오전 11:41
-                            <br /> 채팅 일시
-                          </td>
-                          <td className="flex w-40 items-center justify-center px-1 py-3">
-                            System
-                          </td>
-                          <td className="flex flex-1 items-center truncate px-1 py-3">
-                            채팅 내용 {id_chatlist}
-                          </td>
-
-                          <td className="flex w-24 items-center justify-center truncate px-1 py-3">
-                            Y
-                          </td>
-                        </tr>
-                      ) : (
-                        <tr
-                          role="row"
-                          className="flex border-b hover:bg-blue-100"
-                        >
-                          <td className="flex w-64 items-center justify-center px-1 py-3">
-                            2024년 04월 01일 오전 11:41
-                            <br /> 채팅 일시
-                          </td>
-                          <td className="flex w-40 items-center justify-center px-1 py-3">
-                            System
-                          </td>
-                          <td className="flex flex-1 items-center truncate px-1 py-3">
-                            채팅 내용 {id_chatlist}
-                          </td>
-
-                          <td className="flex w-24 items-center justify-center truncate px-1 py-3">
-                            N
-                          </td>
-                        </tr>
-                      ),
-                    )}
+                  {sortedChats.map((value, index) => (
+                    <tr key={index} role="row" className="flex cursor-pointer border-b hover:bg-blue-100" style={{ height: 'auto' }} >
+                      <td className="flex w-64 justify-center px-1 py-3">
+                        {value.time[0]}-{value.time[1]}-{value.time[2]}
+                      </td>
+                      <td className="flex w-40 justify-center px-1 py-3">
+                        {value.user}
+                      </td>
+                      <td className="flex-1 truncate px-1 py-3">
+                        {value.question} / {value.answer}
+                      </td>
+                      <td className="flex w-24 justify-center truncate px-1 py-3">
+                        {value.answer !== WrongAnswerK && value.answer !== WrongAnswerE ? 'Y' : 'N'}
+                      </td>
+                    </tr>
+                  ))}
+                    
                   </div>
                 </tbody>
               </table>
